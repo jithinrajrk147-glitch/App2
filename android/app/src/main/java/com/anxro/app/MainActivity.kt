3fef5aed1de274953dc0f45867ede1ba50825688
jithinrajrk147-glitch <jithinrajrk147@gmail.com>
2026-05-25 10:44:21 +0530

    Update MainActivity.kt

--- android/app/src/main/java/com/anxro/app/MainActivity.kt
+++ android/app/src/main/java/com/anxro/app/MainActivity.kt
@@ -22,7 +22,14 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-
+     requestPermissions(
+        arrayOf(
+          android.Manifest.permission.RECORD_AUDIO,
+          android.Manifest.permission.POST_NOTIFICATIONS,
+          android.Manifest.permission.READ_EXTERNAL_STORAGE
+         ),
+        1
+       )
         val webView = WebView(this)
 
         setContentView(webView)
